fe95d5887f55438ab677308c59ca3b8cb2445417
Noe Leonel Rosado Quintal <leonelrosado2407@gmail.com>
2024-03-04 18:30:09 -0600

    add layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+{{-- aquí peguen una plantilla  --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    @yield('extra-css')
+</head>
+<body>
+{{-- incluyan el navbar --}}
+@include('layouts.navbar')
+<main>
+    @yield('content')
+</main>
+    @yield('extra-js')
+</body>
+</html>
---
+++ resources/views/pages/example.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+ 
+@section('extra-css')
+    
+@endsection
+@section('content')
+    <h1>Example Page</h1>
+    <p>This is an example page.</p>
+@endsection
+
+@section('extra-js')
+    
+@endsection
