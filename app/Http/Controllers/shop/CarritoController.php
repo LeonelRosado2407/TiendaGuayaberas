b508aa57c4cfa8a3d1e2af531f422352fac62f28
Noe Leonel Rosado Quintal <leonelrosado2407@gmail.com>
2024-03-05 18:15:04 -0600

    Add image retrieval logic to CarritoController

--- app/Http/Controllers/shop/CarritoController.php
+++ app/Http/Controllers/shop/CarritoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\shop;
 use App\Http\Controllers\Controller;
 use App\Models\Productos;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CarritoController extends Controller
 {
@@ -18,9 +19,35 @@ class CarritoController extends Controller
     public function mostrarproductos(){
 
         $productos = Productos::all();
+        
+        foreach ($productos as $producto) {
+            $producto->foto = $this->getImage($producto->id_producto);
+        }
 
-        dd($productos);
         return view('pages.shop.productoslist', $productos);
+    }
+
+    public function getImage($id){
+        $producto = Productos::where('id_producto',$id)->first();
+        if ($producto != null) {
+            $path = $producto->foto;
+            $extension = explode('.',$path)[1];
+            $imagenUrl = Storage::disk('public')->get($path);
+            if ($imagenUrl != null) {
+                $imagenUrl = 'data:image/'.$extension.';base64,'.base64_encode($imagenUrl);
+            }
+            else{
+                $imagenUrl = asset('images/default.png');
+                $extension = 'png';
+                $imagenUrl = 'data:image/'.$extension.';base64,'.base64_encode($imagenUrl);
+            }
+        }else{
+            $imagenUrl = asset('images/default.png');
+            $extension = 'png';
+            $imagenUrl = 'data:image/'.$extension.';base64,'.base64_encode($imagenUrl);
+        }
+
+        return $imagenUrl;
     }
 
 
