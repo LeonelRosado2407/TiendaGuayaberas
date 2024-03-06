a6b6e53d49de7f1b91e8bad874f3495a7ebf78c4
AdolfoVaz <146690208+AdolfoVaz@users.noreply.github.com>
2024-03-05 18:14:59 -0600

    ddd

--- app/Http/Controllers/shop/CarritoController.php
+++ app/Http/Controllers/shop/CarritoController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers\shop;
 
 use App\Http\Controllers\Controller;
 use App\Models\Productos;
+use App\Models\carrito;
 use Illuminate\Http\Request;
+use App\Http\Controllers\shop\ProductosController;
 
 class CarritoController extends Controller
 {
     //
     public function mostrarcarrito(){
         
+        $micarrito = Carrito::all();
+
+        dd($micarrito);
 
         return view('pages.shop.carrito');
     }
@@ -18,9 +23,19 @@ class CarritoController extends Controller
     public function mostrarproductos(){
 
         $productos = Productos::all();
-
+        $productoController = new ProductosController();
+        foreach ($productos as $producto) {
+            $productoImg = $productoController->getImage($producto);
+            dd($productoImg);
+            $producto->foto = $productoImg->foto;
+        }
         dd($productos);
         return view('pages.shop.productoslist', $productos);
+    }
+
+    public function agregarcarrito(){
+
+        return view('pages.Home.home');
     }
 
 
